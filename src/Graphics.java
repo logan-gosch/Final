c76a885c7caf3cad83a73c0f58aa5b3f464c2f70
logan-gosch <54542381+logan-gosch@users.noreply.github.com>
2019-10-09 09:50:42 -0500

    bruh

---
+++ src/Graphics.java
@@ -0,0 +1,14 @@
+public class Graphics {
+    public static void main(String[] args){
+        System.out.println(BBBBBBBBBBBBBBBB    LLLLL              AAAAAA        CCCCCCCCCCCCCC   KKKKKK      KKKK
+                         BBBBBBBBBBBBBBBB    LLLLL             AAAAAAAA       CCCCCCCCCCCCCC   KKKKKK     KKKK
+                         BBB           BB    LLLLL            AA      AA      CCC              KKKKKK    KKKK
+                        BBB           BB    LLLLL           AA        AA     CCC              KKKKKK   KKKK
+                        BBB           BB    LLLLL          AAAAAAAAAAAAAA    CCC              KKKKKK  KKKK
+                        BBBBBBBBBBBBBBBB    LLLLL          AA          AA    CCC              KKKKKK KKKK
+                        BBB            BB   LLLLL          AA          AA    CCC              KKKKKK KKKK
+                         BBB            BB   LLLLL          AA          AA    CCC              KKKKKK  KKKK
+                        BBB            BB   LLLLLLLLLLL    AA          AA    CCCCCCCCCCCCC    KKKKKK   KKKK
+                         BBBBBBBBBBBBBBBBB   LLLLLLLLLLL    AA          AA    CCCCCCCCCCCCC    KKKKKK    KKKK   );
+    }
+}
