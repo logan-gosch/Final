a0e4e8d145f9afb7fdb05a25b945878ab13d0093
logan-gosch <54542381+logan-gosch@users.noreply.github.com>
2019-10-09 21:16:26 -0500

    finished intro graphics

--- src/Graphics.java
+++ src/Graphics.java
@@ -1,14 +1,25 @@
 public class Graphics {
     public static void main(String[] args){
-        System.out.println(BBBBBBBBBBBBBBBB    LLLLL              AAAAAA        CCCCCCCCCCCCCC   KKKKKK      KKKK
-                         BBBBBBBBBBBBBBBB    LLLLL             AAAAAAAA       CCCCCCCCCCCCCC   KKKKKK     KKKK
-                         BBB           BB    LLLLL            AA      AA      CCC              KKKKKK    KKKK
-                        BBB           BB    LLLLL           AA        AA     CCC              KKKKKK   KKKK
-                        BBB           BB    LLLLL          AAAAAAAAAAAAAA    CCC              KKKKKK  KKKK
-                        BBBBBBBBBBBBBBBB    LLLLL          AA          AA    CCC              KKKKKK KKKK
-                        BBB            BB   LLLLL          AA          AA    CCC              KKKKKK KKKK
-                         BBB            BB   LLLLL          AA          AA    CCC              KKKKKK  KKKK
-                        BBB            BB   LLLLLLLLLLL    AA          AA    CCCCCCCCCCCCC    KKKKKK   KKKK
-                         BBBBBBBBBBBBBBBBB   LLLLLLLLLLL    AA          AA    CCCCCCCCCCCCC    KKKKKK    KKKK   );
+        System.out.println("BBBBBBBBBBBBBBBB     LLLLL              AAAAAA        CCCCCCCCCCCCCC   KKKKKK      KKKK ");
+        System.out.println("BBBBBBBBBBBBBBBB     LLLLL             AAAAAAAA       CCCCCCCCCCCCCC   KKKKKK     KKKK");
+        System.out.println("BBB           BB     LLLLL            AA      AA      CCC              KKKKKK    KKKK");
+        System.out.println("BBB           BB     LLLLL            AA      AA      CCC              KKKKKK   KKKK");
+        System.out.println("BBB           BB     LLLLL            AAAAAAAAAA      CCC              KKKKKK  KKKK");
+        System.out.println("BBBBBBBBBBBBBBBBB    LLLLL            AA      AA      CCC              KKKKKK KKKK");
+        System.out.println("BBB            BB    LLLLL            AA      AA      CCC              KKKKKK   KKKK");
+        System.out.println("BBB            BB    LLLLL            AA      AA      CCC              KKKKKK    KKKK");
+        System.out.println("BBBBBBBBBBBBBBBBB    LLLLLLLLLLLLL    AA      AA      CCCCCCCCCCCCC    KKKKKK     KKKK");
+        System.out.println("BBBBBBBBBBBBBBBBB    LLLLLLLLLLLLL    AA      AA      CCCCCCCCCCCCC    KKKKKK       KKKK");
+
+        System.out.println("             JJJJ      AAAAAA     CCCCCCCCCCCCCC   KKKKK      KKKK");
+        System.out.println("             JJJJ     AAAAAAAA    CCCCCCCCCCCCCC   KKKKK     KKKK");
+        System.out.println("             JJJJ    AA      AA   CC               KKKKK    KKKK");
+        System.out.println("             JJJJ    AA      AA   CC               KKKKK   KKKK");
+        System.out.println("             JJJJ    AAAAAAAAAA   CC               KKKKK  KKKK");
+        System.out.println("             JJJJ    AA      AA   CC               KKKKK KKKK");
+        System.out.println("             JJJJ    AA      AA   CC               KKKKK   KKKK");
+        System.out.println(" JJJJJ      JJJJJ    AA      AA   CC               KKKKK    KKKK");
+        System.out.println("  JJJJJ    JJJJJ     AA      AA   CCCCCCCCCCCCCC   KKKKK     KKKK");
+        System.out.println("   JJJJJJJJJJJ       AA      AA   CCCCCCCCCCCCCC   KKKKK      KKKK");
     }
 }
